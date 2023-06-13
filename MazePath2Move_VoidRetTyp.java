e3b8e38f3cfe700a444542d4333c47b2735c4b53
VARSHA RANI <127414456+VarshaRani9@users.noreply.github.com>
2023-06-13 09:26:29 +0530

    MazePath_VoidReturnTyp

---
+++ MazePath2Move_VoidRetTyp.java
@@ -0,0 +1,22 @@
+// void mazePathProblem(int row , int col, int endRow, int endCol, String result)
+// Step - 1 Move to the Right
+// Step-2 Move to the Down
+
+public class MazePath2Move_VoidRetTyp {
+
+	private static void mazePath(int row, int col, int endRow, int endCol, String res) {
+		if(row == endRow && col == endCol) {
+			System.out.println(res);
+			return;
+		}
+		if(row > endRow || col > endCol) {
+			return;
+		}
+		mazePath(row, col+1,endRow, endCol, res+"R");
+		mazePath(row+1, col,endRow, endCol, res+"D");
+	}
+	public static void main(String[] args) {
+		mazePath(0, 0, 2, 2, "");
+	}
+
+}
